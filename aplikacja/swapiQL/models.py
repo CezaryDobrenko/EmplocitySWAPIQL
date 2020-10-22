89b225c9d6abc71277e31dab5a8a9a9e1049d669
CezaryDobrenko <bonzohaker742@gmail.com>
2020-10-22 13:09:50 +0200

    ADDED models in project and dummy data

--- aplikacja/swapiQL/models.py
+++ aplikacja/swapiQL/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+class People (models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
+
+class Movie(models.Model):
+    title = models.CharField(max_length=50)
+    MainCharacters = models.ManyToManyField(People)
+    year = models.IntegerField()
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ('title',)
+
+class Planet (models.Model):
+    name = models.CharField(max_length=50)
+    population = models.IntegerField()
+    residents = models.ManyToManyField(People)
+    gravity = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ('name',)
